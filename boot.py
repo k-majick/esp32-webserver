36bff4169c60d0bc7423e17d8831f1f02ac90ff6
k-majick <k-majick>
2024-09-18 23:10:16 +0200

    store uid in nvs

--- boot.py
+++ boot.py
@@ -1,5 +1,6 @@
 from machine import Pin
 import network
+import esp32
 import esp
 import gc
 
@@ -16,3 +17,19 @@ led_2 = Pin(13, Pin.OUT)
 led_1.value(1)
 print("Access Point established")
 print(ap.ifconfig())
+
+def check_uid():
+    nvs = esp32.NVS('storage')
+    uid = 1
+    stored_uid = None
+
+    try:
+        stored_uid = nvs.get_i32('uid')
+    except OSError:
+        nvs.set_i32('uid', uid)
+        nvs.commit()
+        stored_uid = nvs.get_i32('uid')
+
+    return stored_uid
+
+print('Device UID:', check_uid())
