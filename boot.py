f7aa5603d65ce77dc53f03e737b233b41e2ee4e0
k-majick <k-majick>
2024-09-22 17:53:21 +0200

    register device

--- boot.py
+++ boot.py
@@ -3,33 +3,41 @@ import network
 import esp32
 import esp
 import gc
-
-esp.osdebug(None)
-gc.collect()
+import random
 
 ap = network.WLAN(network.AP_IF)
-ap.active(True)
-ap.config(essid="ESP32-AP", password="123")
-
+nvs = esp32.NVS("storage")
 led_1 = Pin(2, Pin.OUT)
 led_2 = Pin(13, Pin.OUT)
 
-led_1.value(1)
-print("Access Point established")
-print(ap.ifconfig())
 
-def check_uid():
-    nvs = esp32.NVS('storage')
-    uid = 1
+def config():
+    esp.osdebug(None)
+    gc.collect()
+
+
+def ap_init():
+    ap.active(True)
+    ap.config(essid="ESP32-AP", password="123")
+    led_1.value(1)
+    print(f"Access Point established at {ap.ifconfig()}")
+
+
+def uid_check():
     stored_uid = None
 
     try:
-        stored_uid = nvs.get_i32('uid')
+        stored_uid = nvs.get_i32("device_uid")
     except OSError:
-        nvs.set_i32('uid', uid)
+        print(f"Device UID not found, generating")
+        stored_uid = random.randint(0, 2**31 - 1)
+        nvs.set_i32("device_uid", stored_uid)
         nvs.commit()
-        stored_uid = nvs.get_i32('uid')
 
+    print(f"Device UID {stored_uid}")
     return stored_uid
 
-print('Device UID:', check_uid())
+
+config()
+ap_init()
+uid = uid_check()
--- main.py
+++ main.py
@@ -5,31 +5,60 @@ except:
 from time import sleep
 from template import html_template
 import json
+from boot import ap, network, led_1, led_2, uid
+import ubinascii
+import urequests
 
-esp32_ip = ""
+ip = ""
+mac = ""
+
 
 def urldecode(encoded_str):
     hex_chars = "0123456789ABCDEFabcdef"
     decoded_str = ""
     i = 0
     while i < len(encoded_str):
-        if encoded_str[i] == '%' and i + 2 < len(encoded_str) and encoded_str[i+1] in hex_chars and encoded_str[i+2] in hex_chars:
-            decoded_str += chr(int(encoded_str[i+1:i+3], 16))
+        if (
+            encoded_str[i] == "%"
+            and i + 2 < len(encoded_str)
+            and encoded_str[i + 1] in hex_chars
+            and encoded_str[i + 2] in hex_chars
+        ):
+            decoded_str += chr(int(encoded_str[i + 1 : i + 3], 16))
             i += 3
         else:
             decoded_str += encoded_str[i]
             i += 1
     return decoded_str
 
+
+def register_device(ip, mac):
+    url = "https://f6610b26b565e123ffb2f618a9310644.serveo.net/register_device"
+    print(f"Registering device IP: {ip} MAC: {mac}")
+    headers = {"Content-Type": "application/json"}
+    data = {"ip": ip, "mac": mac}
+    try:
+        response = urequests.post(url, headers=headers, data=json.dumps(data))
+        print(response.text)
+        response.close()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
 def close_ap():
+    global mac
     s.close()
     sleep(1)
     ap.active(False)
     print("Access Point closed")
+    sleep(10)
+    register_device(ip, mac)
     return
 
+
 def connect_to_wifi(ssid, password):
-    global esp32_ip
+    global ip
+    global mac
     station = network.WLAN(network.STA_IF)
     station.active(True)
     station.connect(ssid, password)
@@ -39,17 +68,20 @@ def connect_to_wifi(ssid, password):
         sleep(0.5)
         pass
 
-    esp32_ip = station.ifconfig()[0]
-    print(f"Successfully connected to {ssid} with {esp32_ip}")
+    ip = station.ifconfig()[0]
+    mac = ubinascii.hexlify(station.config("mac"), ":").decode()
     led_2.value(1)
+    print(f"Successfully connected to {ssid}")
+
 
 def web_page(mode=""):
-    global esp32_ip
+    global ip
     if mode == "connected":
         content = f"""
         <h1>Wi-Fi credentials received</h1>
-        <p>Your IP is: {esp32_ip}</p>
-        <p>Hit the button to close AP mode</p>
+        <p>Your IP is: {ip}</p>
+        <p>Use following UID to register your device: {uid}</p>
+        <p>Hit the button to close AP mode.</p>
         <form action="/close_ap" method="post">
           <input type="submit" value="Close">
         </form>
@@ -79,6 +111,7 @@ def web_page(mode=""):
     html = html_template.format(content=content)
     return html
 
+
 def create_socket(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -87,17 +120,30 @@ def create_socket(port):
     except OSError as e:
         if e.errno == 112:
             print("Port is already in use, waiting for it to be released...")
-            time.sleep(5)
+            sleep(5)
             s.bind(("", port))
     s.listen(5)
     print(f"Socket created on port: {port}")
     return s
 
+
 s = create_socket(80)
+
 
 def reinitialize_socket(port):
     global s
     s = create_socket(port)
+
+
+def send_response(conn, content, content_type="application/json"):
+    conn.send("HTTP/1.1 200 OK\n")
+    conn.send(f"Content-Type: {content_type}\n")
+    conn.send("Access-Control-Allow-Origin: *\n")
+    conn.send("Access-Control-Allow-Headers: Content-Type\n")
+    conn.send("Connection: close\n\n")
+    conn.sendall(content)
+    conn.close()
+
 
 def main_loop():
     while True:
@@ -120,45 +166,37 @@ def main_loop():
                 password_end = request.find(" HTTP", password_start)
                 password = request[password_start:password_end]
 
-                try:
-                    ssid = urldecode(ssid)
-                    password = urldecode(password)
-                    connect_to_wifi(ssid, password)
-                    mode = "connected"
+                ssid = urldecode(ssid)
+                password = urldecode(password)
+                connect_to_wifi(ssid, password)
+                mode = "connected"
 
-                except Exception as e:
-                    print(f"Error: {e}")
-
-            if "/close_ap" in request:
+            elif "/close_ap" in request:
                 mode = "close"
 
-            if "/toggle" in request:
+            elif "/ping" in request:
+                response = json.dumps({"device_uid": 1})
+                send_response(conn, response)
+                continue
+
+            elif "/toggle" in request:
                 led_1.value(not led_1.value())
                 led_2.value(not led_2.value())
                 led_status = {"led_1": led_1.value(), "led_2": led_2.value()}
                 response = json.dumps({"mode": mode, "led_status": led_status})
-                conn.send("HTTP/1.1 200 OK\n")
-                conn.send("Content-Type: application/json\n")
-                conn.send("Access-Control-Allow-Origin: *\n")
-                conn.send("Access-Control-Allow-Headers: Content-Type\n")
-                conn.send("Connection: close\n\n")
-                conn.sendall(response)
-                conn.close()
+                send_response(conn, response)
+                continue
+
+            elif "/close_ap" in request:
+                close_ap()
+                reinitialize_socket(80)
                 continue
 
             response = web_page(mode)
-            conn.send("HTTP/1.1 200 OK\n")
-            conn.send("Content-Type: text/html\n")
-            conn.send("Connection: close\n\n")
-            conn.sendall(response)
-            conn.close()
-
-            if "/close_ap" in request:
-                close_ap()
-                reinitialize_socket(80)
-                continue  # Restart the loop after reinitializing the socket
+            send_response(conn, response, content_type="text/html")
 
         except OSError as e:
             print(f"OSError: {e}")
+
 
 main_loop()
